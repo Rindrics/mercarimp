afe3b54a7069ee4329e7118d43dec468a0338cb1
Akira Hayashi <smxshxishxad@me.com>
2022-01-30 05:18:56 +0900

    :recycle: Fixturize instantiation of SearchResult

--- test/mercarimp/test_core.py
+++ test/mercarimp/test_core.py
@@ -2,15 +2,18 @@ import pytest
 from mercarimp.core import SearchCondition
 
 
+@pytest.fixture()
+def search_cond():
+        sc = SearchCondition("hoge")
+        return sc
+
+
 class TestSearchCondition():
-    def test_init(self):
-        searchCondition = SearchCondition("hoge")
-
-        assert searchCondition.keyword == "hoge"
+    def test_init(self, search_cond):
+        assert search_cond.keyword == "hoge"
 
 
-    def test_set_keyword(self):
-        search_condition = SearchCondition("hoge")
-        search_condition.set_keyword("fuga")
+    def test_set_keyword(self, search_cond):
+        search_cond.set_keyword("fuga")
 
-        assert search_condition.keyword == "fuga"
+        assert search_cond.keyword == "fuga"
