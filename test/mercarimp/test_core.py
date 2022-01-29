a349954ef986f89a15ea9c54c81def61754d7c8d
Akira Hayashi <smxshxishxad@me.com>
2022-01-30 06:31:16 +0900

    :pencil2: Document codes

--- test/mercarimp/test_core.py
+++ test/mercarimp/test_core.py
@@ -5,16 +5,22 @@ from mercarimp.core import SearchCondition
 
 @pytest.fixture()
 def search_cond():
+    """instantiate SearchCondition"""
     sc = SearchCondition("hoge")
     return sc
 
 
 class TestSearchCondition:
     def test_init(self, search_cond):
+        """instantiation succeeds"""
+        # GIVEN an initialized SearchCondition
         assert search_cond.keyword == "hoge"
 
 
     def test_set_keyword(self, search_cond):
+        """keyword can be set"""
+        # GIVEN an initialized SearchCondition
         search_cond.set_keyword("fuga")
 
         assert search_cond.keyword == "fuga"
+
